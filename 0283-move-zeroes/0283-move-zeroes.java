40458410bfb6cfdcb033a260afe64a2093c509c9
Ian Cheng <91156531+chengtc-dev@users.noreply.github.com>
2023-10-17 22:42:36 +0800

    Time: 2 ms (68.51%), Space: 44.2 MB (99.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int[] temp = new int[n];
-        int i = 0;
+        int[] copyNums = new int[n];
+        int idx = 0;
+
+        Arrays.fill(copyNums, 0);
 
         for (int num : nums) {
             if (num != 0) {
-                temp[i] = num;
-                i++;
+                copyNums[idx] = num;
+                idx++;
             }
         }
 
-        for (int j = i; j < n; j++) {
-            temp[j] = 0;
-        }
-
-        for (int j = 0; j < n; j++) {
-            nums[j] = temp[j];
+        for (int i = 0; i < n; i++) {
+            nums[i] = copyNums[i];
         }
     }
 }
