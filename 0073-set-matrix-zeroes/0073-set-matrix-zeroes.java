b53cd1f66b7b020c1756c29abd6b4d510c626061
Ian Cheng <91156531+chengtc-dev@users.noreply.github.com>
2024-01-31 23:05:08 +0800

    Time: 3 ms (11.79%), Space: 45.3 MB (61.76%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> row = new HashSet<>();
+        Set<Integer> column = new HashSet<>();
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    column.add(j);
+                }
+            }
+        }
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (row.contains(i) || column.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+}
