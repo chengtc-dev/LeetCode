30e7ee43bd3dafd9f26cf7577e3bbd8b99a310b8
Ian Cheng <91156531+chengtc-dev@users.noreply.github.com>
2024-02-06 01:29:42 +0800

    Time: 19 ms (56.32%), Space: 58.5 MB (5.15%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,14 +3,11 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0 ; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], i);
-            } else {
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
-                map.put(nums[i], i);
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+               return true;   
             }
+            
+            map.put(nums[i], i);
         }
         
         return false;
